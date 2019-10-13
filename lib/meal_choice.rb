8a32ae265ec13a79dd582ab22af25cdcb2f7d09e
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-13 14:14:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,14 @@
 # Your code here
+
+#meal choice (to req = veg1 & veg2 , optional protein = "meat")
+
+
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "what a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+end
+
+meal_choice("peas" , "beans" )
+
+meal_choice("peas", "beans", "beef")
