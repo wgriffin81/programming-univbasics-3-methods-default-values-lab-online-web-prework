21f0529349f255add53b482874c7547e6698e03b
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-13 14:20:34 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,16 @@
 # Your code here
+
+#meal choice (to req = veg1 & veg2 , optional protein = "meat")
+
+
+
+def meal_choice(v1, v2, protein = "meat")
+  v1 = veg1
+  v2 = veg2
+  puts "what a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+end
+
+meal_choice("peas" , "beans" )
+
+meal_choice("peas", "beans", "beef")
